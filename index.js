09fff0a4cc43672e1736ec7aa508c6c1704ebeb4
harsimranjit27 <harsimran2731@gmail.com>
2021-08-11 16:35:40 +0530

    Added functionality to add tasks by pressing enter.Improved UI of task list items.

--- index.js
+++ index.js
@@ -1,29 +1,43 @@
 console.log("index.js");
-let count = 0;
-let textarea_content = document.getElementById("write-tasks");
 
 let btn = document.getElementById("btn");
 
 let parentOfListItems = document.getElementById("tasks-list");
-parentOfListItems.style.border = "2px solid teal";
+// parentOfListItems.style.border = "2px solid teal";
+let inputArea = document.querySelector("#write-tasks");
 
-btn.addEventListener("click",addToDo);
-// Functions
-function addToDo(event) {
+// Event Listeners
+inputArea.addEventListener("keyup",function(event){
+    let todo_text = inputArea.value;
+    if(todo_text && event.keyCode === 13){
+        addTodo();
+    }
+});  
+
+function addTodo(event) {
     
-    let text_val = textarea_content.value;
+        let text_val = inputArea.value;
+        let darkColoredListItem = document.createElement("div");
+        darkColoredListItem.classList.add("dark-bg-div");
 
-    if (text_val) {
-        
         let listItemParentDiv = document.createElement("div");
+        
+        darkColoredListItem.appendChild(listItemParentDiv);
+        
+        darkColoredListItem.style.padding = "0.25% 0.5%";
+        darkColoredListItem.style.margin = "0.5%";
+        // darkColoredListItem.style.backgroundColor = "darkgray";
         listItemParentDiv.classList.add("list-item");
         listItemParentDiv.style.position = "relative";
+        // listItemParentDiv.style.padding = "2%";
+        listItemParentDiv.style.backgroundColor = "white";
         
+
         // 1st div tag
         // creating div tag that holds the ul list containing task text
         let textElemDiv = document.createElement("div");
-        textElemDiv.style.width = "85%";
-        textElemDiv.style.border = "2px solid blue";
+        textElemDiv.style.width = "90%";
+        // textElemDiv.style.border = "2px solid blue";
         textElemDiv.classList.add("text-element-div");
         
         let childTextElem = document.createElement("ul");
@@ -31,14 +45,15 @@ function addToDo(event) {
         childTextElem.classList.add("task-item");
         
         // styling text element having task written in it
-        childTextElem.style.border = "2px dotted green";
+        // childTextElem.style.border = "2px dotted green";
         childTextElem.style.padding = "0%";
-        childTextElem.style.margin = "5%";
+        childTextElem.style.margin = "0.5% 3%";
         childTextElem.style.wordWrap = "break-word";
 
         // adding test value to created element
         childTextElem.innerText = text_val;
-        
+        childTextElem.style.fontSize = "1.5em";
+
         // 2nd div tag
         // Creating and adding div tag to add checkbox and delete button
         let buttonDivForListItems = document.createElement("div");
@@ -47,27 +62,29 @@ function addToDo(event) {
         // styling the div holding checkbox and delete
         buttonDivForListItems.style.position = "absolute";
         buttonDivForListItems.style.width = "12%";
-        buttonDivForListItems.style.top = "30%";
+        buttonDivForListItems.style.top = "10%";
         buttonDivForListItems.style.right = "0%";
-        buttonDivForListItems.style.border = "2px solid blue";
+        // buttonDivForListItems.style.border = "2px solid blue";
         
         // Creating and adding checkbox to buttonDivForListItems
         let childCheckBoxElem = document.createElement("input");
         childCheckBoxElem.type = "checkbox";
         childCheckBoxElem.classList.add("checkBox");
-        childCheckBoxElem.style.height = "1.5em";
-        childCheckBoxElem.style.width = "1.5em";
-        childCheckBoxElem.style.margin = "2%";
+        childCheckBoxElem.style.height = "1.35em";
+        childCheckBoxElem.style.width = "1.35em";
+        // childCheckBoxElem.style.padding = "2%";
         
         buttonDivForListItems.appendChild(childCheckBoxElem);
         // console.log(buttonDivForListItems);  
+
+        
         
         // Creating and adding delete button to buttonDivForListItem
         let childDeleteElem = document.createElement("button");
         childDeleteElem.classList.add("delete-btn");
         childDeleteElem.style.float = "right";
         childDeleteElem.innerText = "X";
-        childDeleteElem.style.margin = "2%";
+        childDeleteElem.style.fontSize = "0.95em";
         childDeleteElem.style.backgroundColor = "white";
         childDeleteElem.style.border = "2px solid white";
         
@@ -77,8 +94,8 @@ function addToDo(event) {
         listItemParentDiv.appendChild(textElemDiv);
         listItemParentDiv.appendChild(buttonDivForListItems);
         
-        parentOfListItems.appendChild(listItemParentDiv);
+        parentOfListItems.appendChild(darkColoredListItem);
         
-        textarea_content.value = "";
-    }
+        inputArea.value = "";
 }
+
